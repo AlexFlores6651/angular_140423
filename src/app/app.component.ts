24f3b0a57403e55d22fb699faab5c79248e0f1a8
AlexFlores6651 <106271632+AlexFlores6651@users.noreply.github.com>
2023-04-14 21:34:29 -0600

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent  {
       console.log(this.httpData);
       this.array=this.httpData.datos;
       console.log(this.array);
+      console.log("Manuel Alejandro Flores Ramirez");
     })
   }
 
